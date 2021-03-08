9747d4f51742f86532dee337f3d840213983dbaf
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-03-08 20:20:05 +0300

    created an interfaces

---
+++ src/components/interfaces.tsx
@@ -0,0 +1,41 @@
+export interface CountryData {
+   name: string,
+   toplevelDomain: [string],
+   alpha2Code: string,
+   alpha3Code: string,
+   callingCodes: [string],
+   capital: string,
+   altSpelling: [string],
+   region: string,
+   subregion: string,
+   population: number,
+   latlng: [number],
+   demonym: string,
+   area: number,
+   gini: number,
+   timezones: [string],
+   border: [string],
+   nativeName: string,
+   numericCode: string,
+   currencies: [{}],
+   languages: [{}],
+   translations: {},
+   flag: string,
+   regionalBlocs: [{}]
+  }
+
+  interface ServiceLoading {
+    status: 'loading';
+  }
+  interface ServiceLoaded<T> {
+    status: 'loaded';
+    payload: T;
+  }
+  interface ServiceError {
+    status: 'error';
+    error: Error;
+  }
+  export type Service<T> =
+    | ServiceLoading
+    | ServiceLoaded<T>
+    | ServiceError;
