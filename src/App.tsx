127128a24364cdf3ff07c52a1369855fd24a91e3
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-03-09 14:52:22 +0300

    used Switch and Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import CountriesList from "./components/CountriesList"
+import CountryDetails from './components/CountryDetails'
+import styled from "styled-components"
 
+const Container = styled.div`
+ background-color: gainsboro;
+`;
+const Title = styled.h1`
+ margin: 0;
+ padding: 1rem;
+`;
 export default function App() {
   return (
-    <div>
-      <h1>Rest Countries</h1>
-      <CountriesList/>
-    </div>
+    <Container>
+      <Title>Rest Countries</Title>
+      <Router>
+        <Switch>
+          <Route exact path="/"><CountriesList/></Route>
+          <Route path="/country/:countryName"><CountryDetails/></Route>
+        </Switch>
+      </Router>
+    </Container>
   )
 }
