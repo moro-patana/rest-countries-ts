669311df0b5609f77130ce67831722db7b2b2765
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-03-09 14:53:28 +0300

    show all the countries in the dom

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,20 +1,62 @@
 import React, { useContext } from 'react'
 import { Context } from "./GlobalContext"
+import styled from "styled-components"
+import { Link } from 'react-router-dom';
 
+
+const Container = styled.div`
+  padding: 1rem;
+  a {
+      text-decoration: none;
+      color: black;
+  }
+`;
+const Image =styled.img`
+width: 100%;
+`;
+const CountryCard = styled.div`
+background-color: white;
+`;
+const Title = styled.h2`
+ margin: 0;
+ padding-top: 30px;
+ padding-right: 50px;
+ padding-left: 50px;
+`;
+const List = styled.ul`
+padding: 50px;
+display: flex;
+flex-direction: column;
+gap: 20px;
+
+`;
+const ListItem = styled.li`
+list-style: none;
+`;
 const CountriesList: React.FC<{}> = () => {
     const { results, error, loading} = useContext(Context);
     
 
     return (
-      <div>
+      <Container>
         {loading && <div>Loading...</div>}
         {!loading && results.map(country => (
-            <div key={country.name}>{country.name}</div>
+            <Link to={`/country/${country.name}`} key={country.name}>
+            <CountryCard key={country.name}>
+                <Image src={country.flag} alt="Flag"/>
+                <Title>{country.name}</Title>
+                <List>
+                    <ListItem>Population: {country.population}</ListItem>
+                    <ListItem>Region: {country.region}</ListItem>
+                    <ListItem>Capital: {country.capital}</ListItem>
+                </List>
+            </CountryCard>
+            </Link>
           ))}
         {error && (
           <div>Error, the backend moved to the dark side.</div>
         )}
-      </div>
+      </Container>
     );
   };
   
